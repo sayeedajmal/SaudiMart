3e174a9796657875ef3ea78f58f2b64adff617d0
sayeedajmal <sayeedajmala06@gmail.com>
2025-05-27 10:02:20 +0530

    Setup of Gateway

---
+++ martgateway/src/main/java/com/sayeed/martgateway/MartGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sayeed.martgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartGatewayApplication.class, args);
+	}
+
+}
