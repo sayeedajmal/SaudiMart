5c7006f92da253fbd510ec27bfae87848f2bf393
sayeedajmal <sayeedajmala06@gmail.com>
2025-05-27 11:31:49 +0530

    removed

--- MartProduct/src/main/java/com/saudiMart/Product/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.saudiMart.Product.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.saudiMart.Product.Model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
